18b840712a4a9ee688d62d37f12ac1d3b086cd43
weiweichou99 <weiwei.chou99@gmail.com>
2026-06-05 22:16:48 +0800

    修改測試test

--- 初階題庫/02-迴圈/c316. 找出任意兩點最遠的距離/C316.cpp
+++ 初階題庫/02-迴圈/c316. 找出任意兩點最遠的距離/C316.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 long long dist2(pair<int,int> a, pair<int,int> b) {
-    long long x = (a.first-b.first)*(a.first-b.first) + (a.second-b.second)*(a.second-b.second);
+    long long x = (a.first - b.first) * (a.first-b.first) + (a.second-b.second) * (a.second-b.second);
     return x;
 }
 
@@ -11,11 +11,9 @@ int main() {
     cin >> n;
 
     vector< pair<int,int> > p(n); //宣告三個空座標點，名稱為p
-
     for(int i = 0; i < n; i++) cin >> p[i].first >> p[i].second;
     
     long long maxD = -1;  //宣告一個變數maxD，初始值為-1，代表目前找到的最大距離的平方
-
     //long long minD = 99999999;
     int ans_i = 0, ans_j = 1; //宣告兩個變數ans_i和ans_j，初始值分別為0和1，代表目前找到的最大距離的兩個點的索引
     
