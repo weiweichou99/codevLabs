879b84cb8bb6f6205f9aa016c85d4490f3879e18
DanielChou <daniel0422@gmail.com>
2026-06-06 20:07:36 +0800

    補充最大值max_element、最小值min_element說明。(內建函數用法)

--- 00-C++語法介紹/12-排序與搜尋/readme.md
+++ 00-C++語法介紹/12-排序與搜尋/readme.md
@@ -18,6 +18,35 @@ sort(v.begin(), v.end());  // 排序 vector
 sort(v.begin(), v.end(), greater<int>());
 ```
 
+## 找最大值、最小值
+
+```cpp
+#include <algorithm>
+
+vector<int> v = {5, 2, 8, 1, 9};
+
+// 方法 1：用內建函數
+// max_element 回傳的是「指向最大值的指標（迭代器）」，不是值本身
+// 所以要加 * 來「取出指標指向的值」（解參考）
+int maxVal = *max_element(v.begin(), v.end());  // 9
+int minVal = *min_element(v.begin(), v.end());  // 1
+
+// 如果不加 *，得到的是位置（迭代器），不是數值
+// auto it = max_element(v.begin(), v.end());  // it 是指標
+// int maxVal = *it;  // 用 * 取出值
+
+// 方法 2：排序後取頭尾
+sort(v.begin(), v.end());
+int minVal = v[0];           // 最小
+int maxVal = v[v.size()-1];  // 最大
+
+// 方法 3：手動迴圈找
+int maxVal = v[0];
+for (int x : v) {
+    if (x > maxVal) maxVal = x;
+}
+```
+
 ## 二分搜尋
 
 資料**已排序**時，可用二分搜尋快速找值。
--- 00-C++語法介紹/12-排序與搜尋/sort_demo.cpp
+++ 00-C++語法介紹/12-排序與搜尋/sort_demo.cpp
@@ -1,37 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
     
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
+    // vector<int> v(n);
+    // for (int i = 0; i < n; i++) {
+    //     cin >> v[i];
+    // }
     
-    // 原始資料
-    cout << "原始: ";
-    for (int x : v) cout << x << " ";
-    cout << endl;
+    // // 原始資料
+    // cout << "原始: ";
+    // for (int x : v) cout << x << " ";
+    // cout << endl;
     
-    // 由小到大排序
-    sort(v.begin(), v.end());
-    cout << "由小到大: ";
-    for (int x : v) cout << x << " ";
-    cout << endl;
+    // // 由小到大排序
+    // sort(v.begin(), v.end());
+    // cout << "由小到大: ";
+    // for (int x : v) cout << x << " ";
+    // cout << endl;
     
-    // 由大到小排序
-    sort(v.begin(), v.end(), greater<int>());
-    cout << "由大到小: ";
-    for (int x : v) cout << x << " ";
-    cout << endl;
+    // // 由大到小排序
+    // sort(v.begin(), v.end(), greater<int>());
+    // cout << "由大到小: ";
+    // for (int x : v) cout << x << " ";
+    // cout << endl;
     
-    // 最大值與最小值
-    cout << "最大值: " << v[0] << endl;
-    cout << "最小值: " << v[n-1] << endl;
+    // // 最大值與最小值
+    // cout << "最大值: " << v[0] << endl;
+    // cout << "最小值: " << v[n-1] << endl;
+
+
+    vector<int> v = {5, 2, 8, 1, 9};
+
+    // 方法 1：用內建函數
+    int maxVal = *max_element(v.begin(), v.end());  // 9
+    int minVal = *min_element(v.begin(), v.end());  // 1
+
+    cout << "最大值: " << maxVal << endl;
+    cout << "最小值: " << minVal << endl;
     
     return 0;
 }
